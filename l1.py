19ea766cf60a17cfd216f43539c32310ad3dfd5c
chae1n <165850154+chae1n@users.noreply.github.com>
2024-10-18 09:27:36 -0700

    Add files via upload

--- l1.py
+++ l1.py
@@ -8,16 +8,16 @@ def maxSum(nums: List[int]) -> int:
         for digit in str(number):
             if int(digit)> biggest_single_digit:
                 biggest_single_digit = int(digit)
-        for number in nums:
-            for digit in str(number):
-                if int(digit) == biggest_single_digit:
-                    if number > largest_number:
-                        second_largest_number = largest_number
-                        largest_number = number
-                    elif number > second_largest_number:
-                        second_largest_number = number
+    for number in nums:
+        for digit in str(number):
+            if int(digit) == biggest_single_digit:
+                if number > largest_number:
+                    second_largest_number = largest_number
+                    largest_number = number
+                elif number > second_largest_number:
+                    second_largest_number = number
     if 0 == second_largest_number:
-            return -1
+        return -1
     return largest_number + second_largest_number
 nums = [2536,1613,3366,162]
 print(maxSum(nums))
